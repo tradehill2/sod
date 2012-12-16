9b9963caf7ed814a9c8c8eeb3c8782f5f0b57f39
Michael Sofaer <github@mike.sofaer.net>
2012-12-16 01:56:57 -0800

    Changes fior Debian ruby

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -5,14 +5,17 @@ apt-get install build-essential bison openssl libreadline6 libreadline6-dev curl
 #rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm || true
 #/usr/bin/yum install gcc gcc-c++ kernel-devel patch make bison openssl libreadline6 libreadline6-devel curl git-core zlib1g zlib1g-devel libssl-devel libxml2-devel libxslt-devel autoconf libc6-devel -y
 
-apt-get install ruby1.9 -y
+apt-get remove ruby1.8 -y
+apt-get remove rubygems1.8 -y
+apt-get install ruby1.9.1-full -y
+REALLY_GEM_UPDATE_SYSTEM=true gem1.9.1 update --system
 
-gem install chef --no-rdoc --no-ri
+gem1.9.1 install chef --no-rdoc --no-ri
 echo 'cookbook_path "/etc/sod/cookbooks"' > /etc/sod/solo.rb
 
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-source /root/.bashrc
+export PATH=$PATH:/var/lib/gems/1.9.1/bin/
 
 echo '{"recipes": ["sod::default"]}' > /etc/sod/cookbooks/sod/sod.json
 chef-solo -c /etc/sod/solo.rb -j /etc/sod/cookbooks/sod/sod.json
