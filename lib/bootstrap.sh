b2f420f71c6e7a691e39d9bedd8e24ad0665779c
Michael Sofaer <github@mike.sofaer.net>
2013-03-04 15:24:57 -0800

    Messing around with compiling Ruby from source

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -5,17 +5,21 @@ apt-get install build-essential bison openssl libreadline6 libreadline6-dev curl
 #rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm || true
 #/usr/bin/yum install gcc gcc-c++ kernel-devel patch make bison openssl libreadline6 libreadline6-devel curl git-core zlib1g zlib1g-devel libssl-devel libxml2-devel libxslt-devel autoconf libc6-devel -y
 
-apt-get remove ruby1.8 -y
-apt-get remove rubygems1.8 -y
-apt-get install ruby1.9.1-full -y
-REALLY_GEM_UPDATE_SYSTEM=true gem1.9.1 update --system
+curl $RUBY > /etc/sod/
+tar -xvf /etc/sod/ruby*
+cd /etc/sod/ruby*
+./configure
+make
+make install
 
-gem1.9.1 install chef --no-rdoc --no-ri
+# REALLY_GEM_UPDATE_SYSTEM=true gem1.9.1 update --system
+
+gem install chef --no-rdoc --no-ri
 echo 'cookbook_path "/etc/sod/cookbooks"' > /etc/sod/solo.rb
 
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-export PATH=$PATH:/var/lib/gems/1.9.1/bin/
+# export PATH=$PATH:/var/lib/gems/1.9.1/bin/
 
 echo '{"recipes": ["sod::default"]}' > /etc/sod/cookbooks/sod/sod.json
 chef-solo -c /etc/sod/solo.rb -j /etc/sod/cookbooks/sod/sod.json
--- lib/sod.rb
+++ lib/sod.rb
@@ -30,7 +30,7 @@ module Sod
       connection.exec! "mv /etc/sod/config #{config["remote_ssh_config_location"]}/config"
       connection.exec! "chown -R root:root #{config["remote_ssh_config_location"]}"
     end
-    command =  "bash -cl '/etc/sod/bootstrap.sh'"
+    command =  "bash -cl 'RUBY=#{config[:ruby_url]} RUBY_HASH=#{config[:ruby_hash]} /etc/sod/bootstrap.sh'"
     puts command
     puts connection.exec! command
   end
